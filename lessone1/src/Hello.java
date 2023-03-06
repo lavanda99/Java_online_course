d7ffbbc6069fbb8a3ae676142d930d00b0cfd7a8
LAVANDA <nerkararyan010@gmail.com>
2023-03-06 22:07:47 +0400

    My first git commit

---
+++ lessone1/src/Hello.java
@@ -0,0 +1,31 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("barev\n"); // \n hajord toxi hamar
+        System.out.println("lesson 1");
+
+        byte x = 127;
+        System.out.println(x); // tpelu hamar
+        short y = -9999;
+        System.out.println(y);
+        int a = 999999999;
+        System.out.println(a);
+        long b = 99999999999999L; // amenamec  popoxakan@ l haskanalu hamar //
+        System.out.println(b);
+        float u = 45.8F; // racional tiv f-haskanalu hamar
+        double k = 99.26D; //amenamec racional arjeq d-haskanalu hamr
+        boolean v = true; // kam chishta kam sxal
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+        char tt = '@'; // simvolneri hamar iranqunen nayev koder
+        String s ="barev lessone 1";
+        System.out.println(s);
+        String firstname = "Vle";
+        String lastName = "Nerararyan";
+        System.out.println(firstname + " " + lastName); // mejtexin@ probeli hamar
+        int l = 12;
+        System.out.println("l = " + l);
+
+
+    }
+}
