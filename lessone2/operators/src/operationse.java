5b714cfae5261cb1a8984cef03cfbffcefd61d0a
LAVANDA <nerkararyan010@gmail.com>
2023-03-06 23:50:42 +0400

    commit push

---
+++ lessone2/operators/src/operationse.java
@@ -0,0 +1,59 @@
+public class operationse {
+    public static void main(String[] args) {
+      //  int x;
+     //   int $x;
+     //   int _x = 9;
+     //   System.out.println(_x);
+     //    int a45 = 99;
+        //System.out.println(a45);
+    //    String firstNameOfTheBesStudents = "Nerkararyan";
+      //  String first_name = "Nerkararyan";
+      // String firstName = "Nerkararyan";
+       // System.out.println(first_name);
+
+
+
+int y;
+        int a = 4, b = 3;
+        int c = a + b;
+        System.out.println(c);
+        c = a - b;
+        System.out.println(c);
+        System.out.println(a*b);
+        int k = 7, l = 5;
+        System.out.println(k/l);
+        System.out.println(k%l);
+        double t = 7.0, i = 5.0;
+        System.out.println(t/i);
+        int x = 5;
+        x++;         // avelacnel 1-ov
+        System.out.println(x);
+        x = 3;
+        y = 4;
+        c = x++ - --y;
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+        System.out.println("c= " + c);
+        x = 7;
+        y = 8;
+        c = ++x+ y--;
+        System.out.println(x++);
+        System.out.println(c);
+        System.out.println(x);
+        System.out.println(--y);
+        System.out.println(y);
+
+       boolean bb = x == 12;
+        System.out.println(bb);
+        System.out.println(x != 9);
+        System.out.println(x > 4);
+        System.out.println(x <= 44);
+        x=4;
+        System.out.println(x % 2 == 0);
+        System.out.println("------------------------");
+        x = 1;
+        System.out.println(x >= 1 && x<= 9);
+
+
+    }
+}
